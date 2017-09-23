524a5a58001e35b29e432145f23dace238eb3dbb
Jorge Segura <george7.7@hotmail.com>
2017-09-23 15:28:22 -0600

    Agrego Solicitud del Usuario

--- src/tmp_script.sh
+++ src/tmp_script.sh
@@ -5,11 +5,28 @@ RED="\033[0;31m"
 YELLOW="\033[0;33m"
 RESET="\033[0m"
 
-# Revisar que sea el usuario Root
-if [[ 0 != $UID  ]]
-then
+# Revisar que sea el usuario no sea Root
+if [[ 0 == $UID  ]]
+	
+	then
         # Inicio el codigo del color con red, luego los elimino con reset
-	echo -e "${RED}Usted Debe Ser Root para Correr este Script${RESET}"
+	echo -e "${RED}Usted No Debe Ser Root para Correr este Script${RESET}"
         exit 1
 fi
-echo Hola $USER 
+
+# Obtencion de informacion
+echo -en "Escriba su nombre completo >"
+read NOMBRE
+echo -en "Escriba su correo electronico >"
+read EMAIL
+
+echo -en "Hola $NOMBRE, este es su correo [${GREEN}$EMAIL${RESET}]? [s/n]: " 
+read RESP
+
+if [[ s == $RESP  ]]
+	
+	then 
+	echo HOLA $NOMBRE, su correo es $EMAIL
+fi
+
+
