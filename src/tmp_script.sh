2511476657a718793c226c52d2532d06f96d6c5f
j@j <george7.7@hotmail.com>
2017-09-23 15:47:20 -0600

    Salvo History Clase 5 + ejemplo de su

--- src/tmp_script.sh
+++ src/tmp_script.sh
@@ -29,4 +29,8 @@ if [[ s == $RESP  ]]
 	echo HOLA $NOMBRE, su correo es $EMAIL
 fi
 
+echo -en "${YELLOW}Agregando Configuracion de git para el usuario $NOMBRE${RESET}"
+sudo su - estudiantes -c "git config --global user.name \"$NOMBRE\""
+sudo su - estudiantes -c "git config --global user.name \"$EMAIL\""
+sudo su - estudiantes -c "git config --global core.editor vim"
 
